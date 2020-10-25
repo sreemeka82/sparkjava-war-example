f573cdca74bba71b3341c41ce1c154a95e7069d4
sreemeka82 <72958787+sreemeka82@users.noreply.github.com>
2020-10-25 14:04:20 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "WELCOME TO MY WORLD nocache");
+		get("/hello", (req, res) -> "WELCOME TO DEVOPS WORLD");
 	}
 }
